8d749ccbc17dc217f2b0c0021fa2f88e67ca7f8c
MaysharaF <mayshara@alunos.utfpr.edu.br>
2022-06-30 13:47:47 -0300

    Criando dtos de operação e resultado

---
+++ spring-calculaator/src/main/java/arquiteturacalculadora/demo/dtos/OperationDto.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arquiteturacalculadora.demo.dtos;
+
+/**
+ *
+ * @author Shen
+ */
+public class OperationDto {
+    public int operation;
+    public float first_number; 
+    public float second_number;     
+}
